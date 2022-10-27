9dc35166b64f3b098489c2a0d84864e925fe66dc
nmk1798 <nmk01071998@gmail.com>
2022-10-27 09:21:24 +0700

    change

--- pages/profile/Profile.tsx
+++ pages/profile/Profile.tsx
@@ -9,7 +9,7 @@ export default function Profile() {
   } = router;
   const IsLogin = isLogin === "true";
 
-  console.log( IsLogin);
+  // console.log( IsLogin);
 
   return (
     <div className="flex flex-col justify-center h-screen items-center">
