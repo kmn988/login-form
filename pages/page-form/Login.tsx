e2aba128fcef7bbe8b1d54b09b5ba21bb6e7c497
khoi <nmk01071998@gmail.com>
2022-11-30 23:57:45 +0700

    ba

--- pages/page-form/Login.tsx
+++ pages/page-form/Login.tsx
@@ -38,7 +38,7 @@ export default function Login() {
         </form>
       </FormProvider>
       <div className="flex justify-center items-center m-5 px-3 border-2 border-gray-300 w-72 h-14 rounded-md ">
-        New to page?es
+        New to page?
         <Link href="Signup">
           <button className=" border-2 border-gray-500 rounded-md mx-3 px-3 transition-all duration-200 hover:bg-stone-500 hover:text-white">
             Sign up
